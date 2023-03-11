86634612fde2b93e8fa87abadda81265eb193f19
Sima <MasyanyaSerafimovich>
2023-03-11 17:12:53 +0300

    some changes

--- specs/user/api.spec.js
+++ specs/user/api.spec.js
@@ -2,7 +2,7 @@ import config from "../../config";
 import user from "../helper/user";
 
 describe('В разделе Authorization and authentication', () => {
-    describe('Метод POST /api/v1/login', () => {
+    describe('В разделе Authorization and authentication метод POST /api/v1/login', () => {
 
         test('получает токен при введении правильных логина и пароля', async () => {
             const res = await user.login(config.credentials_valid);
