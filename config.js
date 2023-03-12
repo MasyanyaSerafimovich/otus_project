774879b45e7e3692935bd82f5c6625de3fc38e55
Sima <MasyanyaSerafimovich>
2023-03-12 03:56:11 +0300

    new code for pipline test 10

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+    url:'https://k-ampus.dev',
+    credentials_valid:{
+        username:'s.bazhenova@tandp.ru',
+        password:'s.bazhenova@tandp.ru'
+    },
+    credentials_invalid_username:{
+        username:'ssss.bazhenova@tandp.ru',
+        password:'s.bazhenova@tandp.ru'
+    },
+    credentials_invalid_password:{
+        username:'s.bazhenova@tandp.ru',
+        password:'ssss.bazhenova@tandp.ru'
+    }
+}
+
+export default config
